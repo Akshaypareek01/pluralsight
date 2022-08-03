f0edd4aa45400dbf3c8db64662a1ef105e16722b
mr-ramzan01 <ramzankhanmansoori0786@gmail.com>
2022-08-04 01:38:22 +0530

    updated

--- landingTop.js
+++ landingTop.js
@@ -1,43 +1,107 @@
 document.getElementById("platform").addEventListener("click",displayplatform);
 document.getElementById("products").addEventListener("click",displayproducts);
 document.getElementById("resources").addEventListener("click",displayresources);
+document.getElementById("signin").addEventListener("click",displaysignin);
 var platform = document.getElementById("platformBackground");
 var products = document.getElementById("productsBackground");
 var resources = document.getElementById("resourcesBackground");
+var signin = document.getElementById("signinBackground");
+
+window.onscroll = function() {scrollRun()};
+function scrollStop() {
+  if (document.documentElement.scrollTop > 0) {
+    true;
+  }
+}
+
+function scrollStop() {
+    if (document.documentElement.scrollTop > 0) {
+      document.documentElement.scrollTop = 0;
+    }
+  }
+
+function crossFn() {
+    if(platform.style.display=="block") {
+        console.log(34);
+        platform.style.display = "none";
+        window.onscroll = function() {scrollRun()};
+    }
+    else if( products.style.display=="block") {
+        products.style.display = "none";
+        window.onscroll = function() {scrollRun()};
+    }
+    else if(resources.style.display=="block") {
+        resources.style.display = "none";
+        window.onscroll = function() {scrollRun()};
+    }
+    else if(signin.style.display=="block") {
+        signin.style.display = "none";
+        window.onscroll = function() {scrollRun()};
+    }
+}
 
 function displayplatform() {
     if(true) {
         products.style.display = "none";
         resources.style.display = "none";
+        signin.style.display = "none";
     }
-    if(platform.style.display == "none") {
-        platform.style.display = "block";
+    if(platform.style.display == "block") {
+        platform.style.display = "none";
+        window.onscroll = function() {scrollRun()};
     }
     else {
-        platform.style.display = "none";
+        platform.style.display = "block";
+        window.onscroll = function() {scrollStop()};
     }
 }
 function displayproducts() {
     if(true) {
         platform.style.display = "none";
         resources.style.display = "none";
+        signin.style.display = "none";
     }
-    if(products.style.display == "none") {
-        products.style.display = "block";
+    if(products.style.display == "block") {
+        products.style.display = "none";
+        window.onscroll = function() {scrollRun()};
     }
     else {
-        products.style.display = "none";
+        products.style.display = "block";
+        window.onscroll = function() {scrollStop()};
     }
 }
 function displayresources() {
     if(true) {
         products.style.display = "none";
         platform.style.display = "none";
+        signin.style.display = "none";
     }
-    if(resources.style.display == "none") {
-        resources.style.display = "block";
+    if(resources.style.display == "block") {
+        resources.style.display = "none";
+        window.onscroll = function() {scrollRun()};
     }
     else {
+        resources.style.display = "block";
+        window.onscroll = function() {scrollStop()};
+    }
+}
+function displaysignin() {
+    if(true) {
+        products.style.display = "none";
+        platform.style.display = "none";
         resources.style.display = "none";
     }
+    if(signin.style.display == "block") {
+        signin.style.display = "none";
+        window.onscroll = function() {scrollRun()};
+    }
+    else {
+        signin.style.display = "block";
+        window.onscroll = function() {scrollStop()};
+    }
+}
+
+function displaysigninPage() {
+    console.log(23);
+    location.href="./signin&signuppage/signin.html";
 }
--- signin&signuppage/signin.js
+++ signin&signuppage/signin.js
@@ -1,24 +1,34 @@
 
-document.querySelector("form").addEventListener("submit", addcrd);
+// document.querySelector("form").addEventListener("submit", addcrd);
 var arr=JSON.parse(localStorage.getItem("login"))||[];
-console.log(arr);
 function addcrd(){
     event.preventDefault();
 
     var email=document.querySelector("#email").value;
     var password=document.querySelector("#password").value;
-    // console.log( email, password);
+    console.log( email, password);
     var ans = false;
     for(var i=0; i<arr.length; i++) {
         if(arr[i].email==email && arr[i].password == password) {
             ans = true;
+            break;
         }
         
     }
-    if(ans) {
+    if(email =="" || password == "") {
+        alert("You have written incorrect email or password");
+    }
+    else if(ans) {
         alert("Login Successful");
+        location.href="../landingTop.html"
     }else {
         alert("You have written incorrect email or password");
     }
 
+}
+
+document.getElementById("account").addEventListener("click",displaySignup)
+function displaySignup() {
+    event.preventDefault();
+    location.href="./signup.html";
 }
